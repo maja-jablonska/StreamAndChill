1505ea77cc720f7fd58fa2df7615d112168bdc92
Maja Jabłońska <majajjablonska@gmail.com>
2021-09-25 16:46:56 +0200

    Danceable/nondanceable api

--- audio_analyzer/audio_extractor.py
+++ audio_analyzer/audio_extractor.py
@@ -1,46 +1,19 @@
 from __future__ import unicode_literals
 
-from typing import Dict
-
-import youtube_dl
-
-from bpm import detect_bpm
+from pytube import YouTube
+from bpm import danceable
 
 import os
 cwd = os.getcwd()
 
 
-def get_temp_filename() -> str:
-    for item in os.listdir(os.curdir):
-        if item.endswith(".mp3"):
-            return item
-
-
-def remove_temp_files():
-    for item in os.listdir(os.curdir):
-        if item.endswith(".mp3"):
-            os.remove(item)
-
-
 def extract_audio(ytpath: str) -> str:
-    def my_hook(d):
-        if d['status'] == 'finished':
-            print('Done downloading, now converting ...')
 
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
-        'progress_hooks': [my_hook],
-    }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([ytpath])
-        print(ydl)
+    yt: YouTube = YouTube(ytpath)
+    video = yt.streams.filter(only_audio=True).first()
+    video.download(output_path='./', filename='tmp.mp4')
 
-    specs: Dict[str, float] = detect_bpm(get_temp_filename())
+    d = danceable('./tmp.mp4')
+    os.remove('./tmp.mp4')
 
-    remove_temp_files()
-    return str(specs)
+    return str(d)
--- audio_analyzer/bpm.py
+++ audio_analyzer/bpm.py
@@ -1,11 +1,30 @@
 import essentia.standard as es
-from typing import Dict
+
+SAMPLE_RATE: int = 44100
+
+FRAME_LENGTH: int = 30
+
+# If something is chill for 5 minutes, we decide it's chill.
+MIN_CHILL_TIME: int = 10*FRAME_LENGTH
+HOP_SIZE: int = int()
+
+CHILL_DANCEABLE_LOW_THRESHOLD: float = 1.25
 
 
-def detect_bpm(filepath: str) -> Dict[str, float]:
-    print("Detecting bpm...")
-    features, features_frames = es.MusicExtractor(rhythmStats=['mean', 'stdev'])(filepath)
-    return {"bpm": float(features["rhythm.bpm"]),
-            "beats_count_per_length": float(features["rhythm.beats_count"]) / float(
-                features["metadata.audio_properties.analysis.length"]),
-            "danceability": float(features["rhythm.danceability"])}
+def danceable(filepath: str) -> bool:
+    audio = es.MonoLoader(filename=filepath)()
+    elapsed: int = 0
+
+    for frame in es.FrameGenerator(audio, SAMPLE_RATE*FRAME_LENGTH):
+        print("Detecting danceability...")
+        danceability, _ = es.Danceability()(frame)
+        if danceability > CHILL_DANCEABLE_LOW_THRESHOLD:
+            print(f'Oh! {danceability}')
+            return True
+
+        elapsed += FRAME_LENGTH
+
+        print(danceability)
+        if elapsed > MIN_CHILL_TIME:
+            return False
+
