2e02f8084954ddb1149d792be4f73461d3997523
Verena Schlott <verena.schlott@netlight.com>
2021-09-25 22:59:52 +0200

    feat: added next step in animation

--- youtube-chrome-extension/src/Overlay/Overlay.tsx
+++ youtube-chrome-extension/src/Overlay/Overlay.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react";
+import { BreathingAnimation } from "../BreathingAnimation/BreathingAnimation";
 import { CountdownOverlay } from "../CountdownOverlayCSS/CountdownOverlay";
 import { GetReadyOverlay } from "../GetReadyOverlay/GetReadyOverlay";
 
 import "./Overlay.css";
 const DURATION_STEP_ONE = 6500;
-const DURATION_STEP_TWO = 7000;
-const DURATION_STEP_THREE = 3500;
+const DURATION_STEP_TWO = 7000 + DURATION_STEP_ONE;
+const DURATION_STEP_THREE = DURATION_STEP_TWO + 3500;
+const DURATION_STEP_FOUR = DURATION_STEP_THREE + 10000;
 
 export const Overlay = () => {
   const textStepOne = "It's time to focus on your health";
@@ -14,11 +16,9 @@ export const Overlay = () => {
 
   useEffect(() => {
     setTimeout(() => setStep(2), DURATION_STEP_ONE);
-    setTimeout(() => setStep(3), DURATION_STEP_ONE + DURATION_STEP_TWO);
-    setTimeout(
-      () => setStep(4),
-      DURATION_STEP_ONE + DURATION_STEP_TWO + DURATION_STEP_THREE
-    );
+    setTimeout(() => setStep(3), DURATION_STEP_TWO);
+    setTimeout(() => setStep(4), DURATION_STEP_THREE);
+    setTimeout(() => setStep(4), DURATION_STEP_FOUR);
   }, []);
 
   return (
@@ -26,6 +26,7 @@ export const Overlay = () => {
       {step === 1 && <GetReadyOverlay text={textStepOne} />}
       {step === 2 && <GetReadyOverlay text={textStepTwo} />}
       {step === 3 && <CountdownOverlay />}
+      {step === 4 && <BreathingAnimation />}
     </div>
   );
 };
