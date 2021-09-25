a8892ced36432e8b371b91371aeae3070e26bc2a
Maja Jabłońska <majajjablonska@gmail.com>
2021-09-25 23:30:15 +0200

    Change response to valid json

--- audio_analyzer/audio_extractor.py
+++ audio_analyzer/audio_extractor.py
@@ -1,27 +1,25 @@
 from __future__ import unicode_literals
 
-from typing import List, Tuple, Dict
+from typing import Any, List, Dict
 
 from pytube import YouTube
 from manual_analysis import danceability_timestamps
 
-
-
 import os
+
 cwd = os.getcwd()
 
 
-def extract_audio(ytpath: str) -> str:
-
+def extract_audio(ytpath: str) -> Dict[str, Any]:
     try:
         yt: YouTube = YouTube(ytpath)
         video = yt.streams.filter(only_audio=True).first()
         video.download(output_path='./', filename='tmp.mp4')
 
-        d: List[Tuple[Tuple[int, int], Dict[str, float]]] = danceability_timestamps('./tmp.mp4')
+        d: List[Dict[str, Any]] = danceability_timestamps('./tmp.mp4')
         os.remove('./tmp.mp4')
 
-        return str(d)
+        return {"data": d}
     except Exception as e:
         print(e)
-        return {}
+        return {"data": []}
