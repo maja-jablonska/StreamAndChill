6777b237f6c48c4a4952ae99ee9139376b5bb24f
Maja Jabłońska <majajjablonska@gmail.com>
2021-09-25 22:05:57 +0200

    Add try/except

--- audio_analyzer/audio_extractor.py
+++ audio_analyzer/audio_extractor.py
@@ -11,11 +11,14 @@ cwd = os.getcwd()
 
 def extract_audio(ytpath: str) -> str:
 
-    yt: YouTube = YouTube(ytpath)
-    video = yt.streams.filter(only_audio=True).first()
-    video.download(output_path='./', filename='tmp.mp4')
+    try:
+        yt: YouTube = YouTube(ytpath)
+        video = yt.streams.filter(only_audio=True).first()
+        video.download(output_path='./', filename='tmp.mp4')
 
-    d: List[Tuple[Tuple[int, int], Dict[str, float]]] = danceability_timestamps('./tmp.mp4')
-    os.remove('./tmp.mp4')
+        d: List[Tuple[Tuple[int, int], Dict[str, float]]] = danceability_timestamps('./tmp.mp4')
+        os.remove('./tmp.mp4')
 
-    return str(d)
+        return str(d)
+    except Exception:
+        return {}
