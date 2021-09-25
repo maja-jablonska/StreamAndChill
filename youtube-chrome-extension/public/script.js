98fafb78e0e2dd91a6eb776725b6c0f8f3de1e2e
Verena Schlott <verena.schlott@netlight.com>
2021-09-25 10:47:50 +0200

    feat: added cra with chrome extension insidegsgs

---
+++ youtube-chrome-extension/public/script.js
@@ -0,0 +1,48 @@
+chrome.runtime.onMessage.addListener((message, _sender, _sendResponse) => {
+  console.log("message :>> ", message);
+  console.log("_sender :>> ", _sender);
+  console.log("_sendResponse :>> ", _sendResponse);
+  changeSubtitlesStyle(message.vPos, message.fSize, message.fColor);
+});
+
+changeSubtitlesStyle = (vPos, fSize, fColor) => {
+  console.log("%cnetflix-subtitles-styler : observer is working... ");
+  callback = () => {
+    // .player-timedText
+    const subtitles = document.querySelector(".player-timedtext");
+    console.log("subtitles :>> ", subtitles);
+    if (subtitles) {
+      subtitles.style.bottom = vPos + "px";
+
+      // .player-timedtext > .player-timedtext-container [0]
+      const firstChildContainer = subtitles.firstChild;
+      if (firstChildContainer) {
+        // .player-timedtext > .player-timedtext-container [0] > div
+        const firstChild = firstChildContainer.firstChild;
+        if (firstChild) {
+          firstChild.style.backgroundColor = "transparent";
+        }
+
+        // .player-timedtext > .player-timedtext-container [1]
+        const secondChildContainer = firstChildContainer.nextSibling;
+        if (secondChildContainer) {
+          for (const span of secondChildContainer.childNodes) {
+            // .player-timedtext > .player-timedtext-container [1] > span
+            span.style.fontSize = fSize + "px";
+            span.style.fontWeight = "normal";
+            span.style.color = fColor;
+          }
+          secondChildContainer.style.left = "0";
+          secondChildContainer.style.right = "0";
+        }
+      }
+    }
+  };
+
+  const observer = new MutationObserver(callback);
+  observer.observe(document.body, {
+    subtree: true,
+    attributes: false,
+    childList: true,
+  });
+};
