79e978d1aac52e5db635d04e557482c1cb70abce
Maja Jabłońska <majajjablonska@gmail.com>
2021-09-25 23:03:24 +0200

    Add cache

--- audio_analyzer/server.py
+++ audio_analyzer/server.py
@@ -1,14 +1,19 @@
+from functools import lru_cache
 from os import environ
 
 from flask import Flask
 from audio_extractor import extract_audio
-
 
 app = Flask(__name__)
 
 
 @app.route("/<video_id>")
 def hello_world(video_id: str):
+    return request_audio_extractor(video_id)
+
+
+@lru_cache(maxsize=32)
+def request_audio_extractor(video_id: str):
     return extract_audio(f'https://www.youtube.com/watch?v={video_id}')
 
 
